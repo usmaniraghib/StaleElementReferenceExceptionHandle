0a4b843248e7eb7baaffdb96e9a535484d954529
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-01 15:35:39 +0530

    StaleElementReferenceExceptionHandle

---
+++ src/com/raghib/selenium/StaleElement.java
@@ -0,0 +1,38 @@
+package com.raghib.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StaleElement {
+	
+	public static String url = "https://www.pavantestingtools.com/";
+	public static String onlineTrainingHeader = "//a[text()='Online Training']";
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get(url);
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		
+		WebElement element = driver.findElement(By.xpath(onlineTrainingHeader));
+		element.click();
+		driver.navigate().back();		
+		Thread.sleep(5000);
+		
+		try {
+			element.click();
+		} catch(StaleElementReferenceException e) {
+			element = driver.findElement(By.xpath(onlineTrainingHeader));
+			element.click();
+		}
+		
+		driver.quit();
+
+	}
+
+}
