e0e0f600f5f7671419e28dcd328fb0c7e5a4dfaa
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:44:14 +0530

    Converted into maven project.

--- src/com/raghib/selenium/StaleElement.java
+++ src/com/raghib/selenium/StaleElement.java
@@ -1,47 +1,51 @@
 package com.raghib.selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class StaleElement {
-	
+public class StaleElement extends BaseClass {
+
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
+
 	public static String url = "https://www.pavantestingtools.com/";
 	public static String onlineTrainingHeader = "//a[text()='Online Training']";
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		driver.get(url);
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
+		driver.get(url);
+
 		Thread.sleep(5000);
-		
 		WebElement element = driver.findElement(By.xpath(onlineTrainingHeader));
 		element.click();
-		driver.navigate().back();		
+		driver.navigate().back();
 		Thread.sleep(5000);
-		
+
 		try {
 			element.click();
-		} catch(StaleElementReferenceException e) {
+		} catch (StaleElementReferenceException e) {
 			/*
 			 * If you uncomment the below line then;
 			 * 
-			 * Exception in thread "main" org.openqa.selenium.StaleElementReferenceException: stale element reference: stale element not found
+			 * Exception in thread "main"
+			 * org.openqa.selenium.StaleElementReferenceException: stale element reference:
+			 * stale element not found
 			 * 
 			 * will gone.
 			 * 
-			 * */
-			
-			//element = driver.findElement(By.xpath(onlineTrainingHeader));
+			 */
+
+			// element = driver.findElement(By.xpath(onlineTrainingHeader));
 			element.click();
 		}
-		
-		driver.quit();
-
+		BaseClass.quitDriver();
 	}
-
 }
